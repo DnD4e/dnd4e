0329b648c7f1403a267187994ba4c53a361ea4b2
craigmpetersou <cp7424@ou.ac.uk>
2022-06-11 21:31:02 +0100

    initial vue app and cleanup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="
+      basis-1/3
+      rounded-sm
+      px-6
+      py-8
+      ring-1 ring-slate-900/5
+      bg-white
+      dark:bg-slate-700
+      shadow-xl
+      my-8
+    "
+  >
+    <h1>DND 4th Edition</h1>
+  </div>
+</template>
+
+<style></style>
