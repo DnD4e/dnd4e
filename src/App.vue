4b76f7b4b6b3e56644d66a95535b3d63a1ac0d76
craigmpetersou <cp7424@ou.ac.uk>
2022-06-12 20:39:06 +0100

    colour fiddle; tailwind as recc

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div
-    class="
-      basis-1/3
-      rounded-sm
-      px-6
-      py-8
-      ring-1 ring-slate-900/5
-      bg-white
-      dark:bg-slate-700
-      shadow-xl
-      my-8
-    "
-  >
-    <h1>DND 4th Edition</h1>
-  </div>
+    <div
+        class="dark:text-slate-400 text-slate-800 basis-1/3 rounded-sm py-6 px-6 ring-1 ring-slate-900/5 bg-slate-200 dark:bg-slate-700 shadow-xl my-8"
+    >
+        <h1>DND 4th Edition</h1>
+    </div>
 </template>
 
 <style></style>
