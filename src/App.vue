5da849cfecdca9ec65f8c7e6385bd79dd82e3c0a
craigmpetersou <cp7424@ou.ac.uk>
2022-06-12 21:16:17 +0100

    move h1 back to css. add ipsum

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <script setup lang="ts"></script>
 
 <template>
+    <!-- TODO: Extract to a component as our base compontent -->
     <div
-        class="dark:text-slate-400 text-slate-800 basis-1/3 rounded-sm py-6 px-6 ring-1 ring-slate-900/5 bg-slate-200 dark:bg-slate-700 shadow-xl my-8"
+        class="dark:text-slate-400 text-slate-800 basis-1/3 py-6 px-6 ring-1 ring-slate-900/5 bg-slate-200 dark:bg-slate-700 shadow-xl my-8 rounded-lg"
     >
-        <h1>DND 4th Edition</h1>
+        <h1>DND 4<sup>th</sup> Edition</h1>
+        <p>
+            Pok pok try-hard portland blue bottle intelligentsia. Taxidermy
+            scenester franzen humblebrag tilde street art wolf chartreuse
+            authentic williamsburg marfa occupy. Hell of plaid authentic subway
+            tile next level, seitan whatever beard snackwave prism cardigan
+            raclette. Jianbing semiotics affogato, tattooed skateboard
+            humblebrag post-ironic gastropub cold-pressed craft beer trust fund
+            tilde listicle bushwick bitters. Aesthetic polaroid af, poke tousled
+            marfa trust fund live-edge knausgaard cray. Swag meditation tonx,
+            bitters before they sold out fingerstache gastropub.
+        </p>
     </div>
 </template>
 
 <style lang="postcss">
-/* Global Stypes using Tailwind CSS */
+body {
+    @apply bg-white dark:bg-slate-800;
+}
 
-h1 {
-    @apply subpixel-antialiased text-2xl font-bold;
+#app {
+    @apply flex items-center justify-center;
 }
 </style>
