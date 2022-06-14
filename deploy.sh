e0f553576d73d0919351b730aaf1ac5b88544d9f
Craig Peters <craigpeters@me.com>
2022-06-14 18:04:45 +0100

    change to master as github pages branch

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:dnd4e/dnd4e.github.io.git main
+git push -f git@github.com:dnd4e/dnd4e.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
